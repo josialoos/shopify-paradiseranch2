b4ba68741246ed77a8063af21a2289f7ba682ac6
danielloos36 <daniel.loos@yahoo.de>
2022-03-24 14:42:37 +0100

    update

--- assets/paradise-custom.js
+++ assets/paradise-custom.js
@@ -1,12 +1,12 @@
 
 //link deaktivieren in Blogübersicht
 
-if (window.matchMedia('(max-width: 1025px)').matches)
-{
-    var x = document.getElementById("shopify-section-blog-two").querySelectorAll("a");
+// if (window.matchMedia('(max-width: 1025px)').matches)
+// {
+//    var x = document.getElementById("shopify-section-blog-two").querySelectorAll("a");
 
-    var i;
-    for (i = 0; i < x.length; i++) {
-      x[i].removeAttribute("href");
-    };
-}
+//    var i;
+//    for (i = 0; i < x.length; i++) {
+//      x[i].removeAttribute("href");
+//    };
+//}
